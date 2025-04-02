f7d7d182ba4948bcd1b0aa628fd1996ae7831ba7
MohamedRizwan2214 <MohamedRizwan2214@gmail.com>
2025-04-02 19:02:07 +0530

    Task 4

---
+++ Task 4 Weather App with API Integration/main.js
@@ -0,0 +1,35 @@
+// const dotenv = require("dotenv");
+
+// dotenv.config();
+const city = document.getElementById("city");
+const apiKey ="b00ad96ef3951c1330c4268668bda875";
+
+async function fetchWeatherData(apiURL) {
+  try {
+    const response = await fetch(apiURL);
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    const data = await response.json();
+    console.log(data);
+    document.getElementById("cityDetails").innerHTML = data.name;
+    document.getElementById("temp").innerHTML = data.main.temp + "°C";
+    document.getElementById("humidity").innerHTML = data.main.humidity + "%";
+    document.getElementById("wind").innerHTML = data.wind.speed + " m/s";
+    document.getElementById("pressure").innerHTML = data.main.pressure + " pa";
+    document.getElementById("weather").style.display = "block";
+  } catch (error) {
+    // console.error("There was a problem with the fetch operation:", error);
+    document.getElementById("weather").style.display = "none";
+    alert("Please enter a valid city name.");
+  }
+}
+
+function getWeather() {
+  const cityName = city.value;
+  const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+  fetchWeatherData(apiURL);
+
+  //   console.log(cityName);
+}
---
+++ Task 4 Weather App with API Integration/readme.md
@@ -0,0 +1,22 @@
+# Task 4: Weather App with API Integration
+
+## Objective
+
+Create a weather app that fetches and displays weather information based on a city name input.
+
+![Weather App Screenshot 2](image-1.png)
+![Weather App Screenshot 1](image.png)
+
+---
+
+## Key Outcomes
+
+- **Fetch Data from an API**: Learn how to use the `fetch` API to retrieve data from an external source and handle responses effectively.
+- **Error Handling**: Understand how to manage network errors and handle invalid user inputs gracefully.
+- **Dynamic Data Display**: Gain experience in updating the webpage dynamically using DOM manipulation.
+- **API Integration**: Work with the OpenWeatherMap API to fetch real-time weather data.
+- **User-Friendly Interface**: Build an intuitive interface for users to input city names and view weather details.
+- **Template Literals**: Explore the use of template literals to construct API URLs dynamically.
+- **JavaScript Features**: Improve your understanding of modern JavaScript features like `async/await`.
+
+---
